f8451a031f615fda213e37c1cf1485ba3b3443de
Lukas Belz <Lukas.Belz@itacsoftware.com>
2021-12-02 10:59:21 +0100

    diesdas

--- Weatherapp/script.js
+++ Weatherapp/script.js
@@ -9,8 +9,8 @@ let weather = {
             )
             .then((response) => {
                 if (!response.ok) {
-                    alert("No weather found.");
-                    throw new Error("No weather found.");
+                    alert("Kein Wetter gefunden.");
+                    throw new Error("Kein Wetter gefunden.");
                 }
                 return response.json();
             })
